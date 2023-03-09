b9a124a634ac24937190fc76c9f46eef1c07fd0a
YKWTII0 <bonggutravel@gmail.com>
2023-03-09 14:34:46 +0900

    sum method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+    public static int sum(int x, int y){
+        return x + y;
+    }
+    // Run start at main() method
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int i = 20;
+        int s;
+        char a;
+
+        s = sum(i, 10);
+        a = '?';
+        System.out.println(a);
     }
 }

1308caed616ccef9a0578d3d0e6f282aa2e32686
YKWTII0 <bonggutravel@gmail.com>
2023-03-09 15:45:21 +0900

    Compare Integers

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
-import java.util.Scanner;
+import java.awt.desktop.SystemEventListener;
+import java.util.Scanner; // 만들어져 있는 class를 사용하겠다는 뜻
 // System.in : standard input stream
 // System.out : standard output stream
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); // create Scanner object
-        System.out.print("x값 입력 : ");
-        double x = input.nextDouble();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first integer : ");
+        int num1 = input.nextInt();
+        System.out.println("Enter the second integer : ");
+        int num2 = input.nextInt();
 
-        double y = 3 * Math.pow(x, 2) + 2 * x - 1;
-        System.out.println("y값 : " + y);
+        if (num1 == num2){
+            System.out.println("같다");
+        }
+        else if (num1 > num2){
+            System.out.println("첫번째 숫자가 크다");
+        }
+        else if (num1 < num2){
+            System.out.println("두번쨰 숫자가 크다");
+        }
     }
 }
